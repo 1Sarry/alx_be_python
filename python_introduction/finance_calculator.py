928bb7420679cf3d1fba7d1de3ddda9b4a1d69b1
saronmeles4 <saronmeles4@gmail.com>
2025-05-25 21:23:10 +0300

    Finance Calculator typo error fixed

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,8 @@
 monthly_income = input("Enter your monthly income:")
-monthly_expense = input ("Enter your total monthly expenses:")
+monthly_expenses = input ("Enter your total monthly expenses:")
 
-monthly_savings =float (monthly_income) - float(monthly_expense)
+
+monthly_savings =float(monthly_income) - float(monthly_expenses)
 projected_savings = float(monthly_savings) * 12 + (float(monthly_savings) *12 *0.05)
 
 prfloat(f"Your monthly savings are {monthly_savings}")
