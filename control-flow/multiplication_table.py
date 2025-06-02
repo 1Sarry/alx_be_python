9d0a1210c3b68f515a06076a834bbb1892fcf370
saronmeles4 <saronmeles4@gmail.com>
2025-06-02 04:30:55 +0300

    data type changed to int from float

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = float(input("Enter a number to see its multiplication table:"))
+number = int(input("Enter a number to see its multiplication table:"))
 for x in range (10):
     result = number*x
     print(f"{number}*{x}={result}")
