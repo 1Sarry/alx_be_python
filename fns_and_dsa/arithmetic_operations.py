1545ff393fdfaec1a3576a80c04c379d7aeb9fce
saronmeles4 <saronmeles4@gmail.com>
2025-06-23 12:55:36 +0300

    Arithmetic operation subtract operation edited

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
       result = num1*num2
       return result
    elif operation == "subtract":
-      result = num2- num1
+      result = num1- num2
       return result
    elif operation == "add":
       result = num1 + num2
