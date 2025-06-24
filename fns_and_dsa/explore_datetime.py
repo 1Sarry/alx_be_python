840c8bae52d25b248abab4963a6ee1cf8d06d17c
saronmeles4 <saronmeles4@gmail.com>
2025-06-24 14:50:28 +0300

    Futere date formatted

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 def display_current_datetime():
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current date and time: {current_date}")
+    print(f"Current date and time: {formatted_date}")
     return current_date
 current_date = display_current_datetime()
 
@@ -12,5 +12,6 @@ number_of_days = int(input("Please Enter a number of days: "))
 
 def calculate_future_date(current_date, number_of_days):
     future_date = current_date.date() + timedelta(days=number_of_days)
-    print(f"Future date after {number_of_days} days: {future_date}")
+    formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Future date after {number_of_days} days: {formatted_future_date}")
 future_date = calculate_future_date(current_date, number_of_days)
